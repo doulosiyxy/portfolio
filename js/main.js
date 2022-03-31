ea1afdeccaffa827d0ea5eb463440e675b76c989
James Crooke <jamesrcrooke@gmail.com>
2022-03-31 20:42:44 +0100

    Responsiveness edits
    CSS edits to index. Plus fixed hover overriding animation.

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ $('.four').delay(6100).fadeTo(1500, 0.7);
 $('#logo-icon').animate({height: 220}, 200);
 $('#contact-text').css('padding-bottom','2000px');
 
-$('.project-figure').on('mouseover', function() {
+/*$('.project-figure').on('mouseover', function() {
   $(this).css("opacity", 1).on('mouseleave', function() {
     $(this).css("opacity", 0.7);
   });
-});
+});*/
 
 //hyperlinks
 $('.linkedin').on('click', function() {
@@ -38,11 +38,11 @@ $('.three').on('click', function() {
 });
 $('.four').on('click', function() {
   window.open('http://www.wycliffechurch.org.uk', '_blank');
-}); 
+});
 $('.projects-link').on('click', function() {
   window.location.href="index.html"
-}); 
-  
+});
+
 $('#contact-nav').delay(200).animate({"opacity": 1}, 400);
 $('#about-nav').delay(1500).animate({"opacity": 1}, 400);
 $('#contact-footer').delay(200).animate({"opacity":1}, 400);
@@ -55,7 +55,7 @@ var number = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'n
 var i = 0;
 
 function titleLoop() {
-  setTimeout(function () { 
+  setTimeout(function () {
 	//var word = document.getElementById(number[i]);
 	$('#about-title span').eq(i).fadeTo(2500, 1);
 	i++;
@@ -71,23 +71,23 @@ setInterval(titleLoop(), 800);
 //var numberTwo = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen'];
 var x = 0;
 
-function titleFade() { 
+function titleFade() {
   setTimeout(function () {
   $('#about-title span').eq(x).fadeTo(500, 0);
-  $('.skl').delay(350).eq(x).fadeTo(1900, 1);
+  $('.skl').delay(220).eq(x).fadeTo(1900, 1);
   x++;
   //var stop = 14;
   if ( x < 14) {
 	titleFade();
 	}
-  }, 1) 
+  }, 1)
 }
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) { 
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
   $('#contact-text').css('padding-bottom','2000px');
 
-   
+
 } else if ($(window).height() > 767 ) {
   $('#skill-btn').on('click', function() {
     $('#about-title-wrap').delay(840).animate({"top": "0"}, 2300);
@@ -120,9 +120,9 @@ var colors = new Array(
   [47,144,235],
   [105,95,95],
   [245,245,245]);
-  
+
 var step = 0;
-//color table indices for: 
+//color table indices for:
 // current color left
 // next color left
 // current color right
@@ -133,12 +133,12 @@ var gradientSpeed = 0.002;
 
 function updateGradient() {
   if ( $===undefined ) return;
-  
+
   var c0_0 = colors[colorIndices[0]];
   var c0_1 = colors[colorIndices[1]];
   var c1_0 = colors[colorIndices[2]];
   var c1_1 = colors[colorIndices[3]];
-  
+
   var istep = 1 - step;
   var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
   var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
@@ -153,18 +153,18 @@ function updateGradient() {
   $('.gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  
+
   step += gradientSpeed;
   if ( step >= 1 ) {
     step %= 1;
     colorIndices[0] = colorIndices[1];
     colorIndices[2] = colorIndices[3];
-    
+
     //pick two new target color indices
     //do not pick the same as the current one
     colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
     colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-  
+
   }
 }
 setInterval(updateGradient,15);
