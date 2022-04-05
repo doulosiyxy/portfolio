6da42b5cc14605f46e8f403f40e0f907591a035c
James Crooke <jamesrcrooke@gmail.com>
2022-04-05 18:19:11 +0100

    Apps page update 1

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ $('.three').delay(5900).fadeTo(1500, 0.7);
 $('.four').delay(6100).fadeTo(1500, 0.7);
 $('#logo-icon').animate({height: 220}, 200);
 $('#contact-text').css('padding-bottom','2000px');
+$('.overlay-sunrise').delay(300).fadeTo(400, 0);
+$('.overlay-sunrise').delay(800).css('display', 'none');
+
 
 
 /*
@@ -38,7 +41,7 @@ $('.two').on('click', function() {
   window.open('https://doulosiyxy.github.io/theexegesist', '_blank');
 });
 $('.three').on('click', function() {
-  window.open('http://ornis.co.uk', '_blank');
+  window.location.href="aana.html"
 });
 $('.four').on('click', function() {
   window.open('http://www.wycliffechurch.org.uk', '_blank');
@@ -91,7 +94,6 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
   $('#contact-text').css('padding-bottom','2000px');
 
-
 } else if ($(window).height() > 767 ) {
   $('#skill-btn').on('click', function() {
     $('#about-title-wrap').delay(840).animate({"top": "0"}, 2300);
@@ -100,6 +102,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   $('#skill-btn').on('click', function() {
     $('#about-title-wrap').delay(840).animate({"top": "0"}, 2300);
   });
+
 }
 
 // Digital Clock
@@ -179,3 +182,195 @@ setInterval(updateGradient,15);
     window.location.href='index.html';
   });
 });
+
+//digital clockapp
+
+$(document).ready( function() {
+
+  function displayTime() {
+    var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   	var monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+   	var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+   	var seconds = currentTime.getSeconds();
+   	var changeColor = seconds % 2;
+   	var day = currentTime.getDay();
+   	var date = currentTime.getDate();
+   	var month = currentTime.getMonth();
+   	var year = currentTime.getYear();
+
+   	//below two vars grab array strings to supply dayDiv and dateDiv at bottom.
+    var selectDay = weekDay[day];
+    var selectMonth = monthName[month];
+
+   	//this code turns the clock into a 12 hour with AM/PM
+   	var meridiem = "AM";
+   	if (hours > 12) {
+    hours = hours - 12;
+    meridiem = "PM";
+    }
+
+    // 0 AM and 0 PM should read as 12
+    if (hours === 0) {
+    hours = 12;
+    }
+
+
+    if (changeColor === 0) {
+    $('#all').removeClass('even').addClass('odd');
+    }
+    if (changeColor === 1) {
+    $('#all').removeClass('odd').addClass('even');
+    }
+
+
+    // Below adds 0s to seconds, minutes, hours
+   	if (seconds < 10) {
+    seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+    minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+    hours = "0" + hours;
+    }
+
+   	var clockDiv = document.getElementById('clock');
+   	var dayDiv = document.getElementById('day');
+   	var dateDiv = document.getElementById('date');
+   	clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+    dayDiv.innerText = selectDay;
+    dateDiv.innerText = selectMonth + " " + date + " " + (year + 1900);
+
+  }
+  setInterval(displayTime, 1000);
+});
+
+//sunmoon
+
+$(document).ready(function() {
+    $('#orb').click( function() {
+        if ($('#orb').hasClass('sun-orb')) {
+            $('#orb').removeClass('sun-orb').addClass('moon');
+        }
+        else {
+            $('#orb').removeClass('moon').addClass('sun-orb');
+        }
+        if ($('#sky').hasClass('day-sunmoon')) {
+          $('#sky').removeClass('day-sunmoon').addClass('night-sunmoon');
+        } else {
+          $('#sky').removeClass('night-sunmoon').addClass('day-sunmoon');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+          $('#moonspot1').removeClass('visible');
+        } else {
+          $('#moonspot1').addClass('visible');
+        }
+        if ($('#moonspot2').hasClass('visible')) {
+          $('#moonspot2').removeClass('visible');
+        } else {
+          $('#moonspot2').addClass('visible');
+        }
+        if ($('#moonspot3').hasClass('visible')) {
+        $('#moonspot3').removeClass('visible');
+        } else {
+        $('#moonspot3').addClass('visible');
+        }
+    });
+});
+//orbital
+
+var tl = new TimelineLite();
+
+function start() {
+  var smalls = ["#circleone", "#circlethree", "#circlefive", "#circletwo", "#circlefour",  "#circlesix"];
+  var topSmalls = ["#circleone", "#circlethree", "#circlefive"];
+  var bottomSmalls = ["#circletwo", "#circlefour", "#circlesix"];
+  tl
+    .from(".centre", 0.4, { scale: 0, ease: Back.easeOut})
+    .staggerFrom(topSmalls, .4, {scale: 0, y:100, ease: Power4.easeOut}, 0.1, "smalls")
+    .staggerFrom(bottomSmalls, .4, { scale: 0, y:-100, delay: 0.30, ease: Power4.easeOut}, 0.1, "smalls")
+
+   .to("#orbitOne", .6, { rotation: 180, ease: Power0.easeNone }, "rotate")
+
+    .to("#orbitTwo", .6, { rotation: 240, ease: Power0.easeNone }, "rotate")
+
+    .to("#orbitThree", .6, { rotation: 300, ease: Power0.easeNone }, "rotate")
+
+  .to(".home, .info" , .6, { rotation: -180, ease: Power0.easeNone }, "rotate")
+  .to(".email, .stars", .6, { rotation: 60, ease: Power0.easeNone }, "rotate")
+  .to("explore, .account_circle", .6, { rotation: 120, ease: Power0.easeNone }, "rotate");
+
+}
+
+start();
+
+// initiate reversed animation and button colour change.
+$('.small').on('click', function () {
+  tl.timeScale(1.5).reverse()
+  var colour = $(this).css("background-color");
+    TweenMax.to("#orbitalbutton", .4, {scale:1, backgroundColor:colour, delay: 1.2, ease: Back.easeOut});
+
+});
+
+//restart tl with change of colour.
+function replay() {
+  tl.restart();
+  var colour = $("#orbitalbutton").css("background-color");
+  TweenMax.to("#orbitalbutton", .1, {scale:0,});
+  TweenMax.to(".centre", 0.1, { backgroundColor: colour });
+
+}
+// button movement.
+$('.small').on('mousedown', function () {
+  $(this).css('transform', 'scale(0.9)');
+  $(this).on('mouseup', function() {
+    $(this).css('transform', 'scale(1)');
+  });
+});
+
+//sunrise animations
+
+var tlTwo = new TimelineLite();
+function sunRiseFast() {
+  tlTwo
+    .from(".sun", 2, { y: 300, scale: 0.5, ease: Elastic.easeOut })
+    .from(".night", 1, { opacity: 1, delay: 0.2 }, 0)
+    .from(".tree", 0.2, { rotationX: -90, y: 45, ease: Back.easeOut }, 0.5)
+    .fromTo(
+      ".plane",
+      4,
+      { right: -150, y: 50 },
+      { left: -150, y: 0, ease: SlowMo.ease.config(0.4, 0.7, false) }
+    )
+    .fromTo(
+      ".birds",
+      35,
+      { top: 257, x: 0, scale: 0.5 },
+      { top: -100, x: 600, scale: 1, ease: Power0.easeNone },
+      "bird"
+    )
+    .to(
+      ".birds",
+      15,
+      { scale: 1.75, skewX: "50reg", ease: Circ.easeOut },
+      "bird"
+    )
+
+    .to(
+      ".button-sun-rise",
+      3,
+      { scale: 1.5, opacity: .4, delay: 13, rotation: -360, ease: Power4.easeOut }, "bird"
+    )
+
+    .to(".birds", 0.3, { opacity: 0.6, repeat: 300, yoyo: true }, "bird");
+}
+
+sunRiseFast();
+
+
+function replayTwo() {
+
+  tlTwo.restart();
+}
